93c2b3ddaf47118907faba2a0f64b321c979e686
Digvijay Jondhale <digvijayjondhale623@gmail.com>
2023-04-17 15:53:16 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1600},
-  base: "/Profile",
+  base: "/profile",
   plugins: [react()],
 })
